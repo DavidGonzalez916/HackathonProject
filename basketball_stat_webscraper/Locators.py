12653c88c34a5857a3db2cf1c5dd816812ace39f
DavidGonzalez916 <dagonzalez@deloitte.com>
2024-05-03 11:18:56 -0400

    add locators for ppg, rpg, and apg for individual player profiles.

--- basketball_stat_webscraper/Locators.py
+++ basketball_stat_webscraper/Locators.py
@@ -7,9 +7,21 @@ class Locators:
   
   searchBar = (By.CSS_SELECTOR, ".ac-input.completely")
   table = (By.XPATH, "(//table[@id='tot'])//a")
+  pts = (By.XPATH, "//div[@class='p1']//div[2]/p[2]")
+  trb = (By.XPATH, "//div[@class='p1']//div[3]/p[2]")
+  ast = (By.XPATH, "//div[@class='p1']//div[4]/p[2]")
 
   def search(self):
     return self.driver.find_element(*Locators.searchBar)
   
   def tableOfNames(self):
     return self.driver.find_elements(*Locators.table)
+  
+  def points(self):
+    return self.driver.find_element(*Locators.pts)
+  
+  def rebounds(self):
+    return self.driver.find_element(*Locators.trb)
+  
+  def assists(self):
+    return self.driver.find_element(*Locators.ast)
